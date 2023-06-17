8d6861931ce73c06437d61541d9722e45beae5f3
lukesnyder98 <lukesnyder98@gmail.com>
2023-06-17 11:10:35 -0400

    Updating logic.js to add popups

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -25,7 +25,7 @@ let baseMaps = {
 let map = L.map('mapid', {
     center: [43.7, -79.3],
     zoom: 11,
-    layers: [satelliteStreets]
+    layers: [streets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -34,9 +34,22 @@ L.control.layers(baseMaps).addTo(map);
 // Accessing the Toronto neighborhoods GeoJSON URL.
 let torontoHoods = "https://raw.githubusercontent.com/lukesnyder98/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
+// Create a style for the lines.
+let myStyle ={
+    color: "blue",
+    weight: 1,
+    fillColor: "yellow" 
+}
+
 // Grabbing our GeoJSON data.
 d3.json(torontoHoods).then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data).addTo(map);
+    L.geoJSON(data, {
+        style: myStyle,
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+        }
+    })
+    .addTo(map);
 });
